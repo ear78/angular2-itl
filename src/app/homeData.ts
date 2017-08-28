67c11a258ff9a4ea154bdb11bf00d89ec0cb7ef3
elliot richardson <ear78@aol.com>
2017-08-27 21:56:25 -0500

    added footer styling and setup, fixed outer container for angular 2 setup, working on featured products section styling

--- src/app/homeData.ts
+++ src/app/homeData.ts
@@ -9,14 +9,14 @@ export const HOMES: Home[] = [
         },
         {
             id: 2,
-            title: "Title 2",
-            info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            title: "ILS-S810 Temporary Solar Light",
+            info: "ITL's new fully enclosed solar powered temporary obstruction light.",
             image: "/assets/img/ILS-S810-000_CLOSED_1_T_1000.png"
         },
         {
             id: 3,
-            title: "Title 3",
-            info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            title: "ILS-3600 Dual LED Lighting System",
+            info: "ITL's new dual LED lighting system includes Infrared LED operation and optional internal monitoring for advanced diagnostics.",
             image: "/assets/img/ILS-3600-000_CLOSED_1_T_1000.png"
         },
     ]
